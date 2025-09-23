e9974d0ea9993ce866b3b60fdcc463dce7ee27a2
Charlie <CharlieStrachan00@gmail.com>
2025-09-23 20:18:46 +0100

    Updated words.json

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
 
-// const List<Color> colors = [
-//   Color.fromARGB(255, 254, 250, 224), // Background Color
-//   Color.fromARGB(255, 233, 237, 201), // Container Color
-//   Color.fromARGB(255, 204, 213, 174), // Widget Color
-//   Color.fromARGB(255, 106, 82, 57), // Font Color
-// ];
-
 const Map<String, Color> colors = {
   "backgroundColor": Color.fromARGB(255, 254, 250, 224),
   "containerColor": Color.fromARGB(255, 233, 237, 201),
